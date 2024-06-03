f64b0508115e06f72415779acb16dc135ed5fc2b
Damar2121 <damar.vivoy51@gmail.com>
2024-06-03 10:25:22 +0700

    library

--- ioFileWithpath/ioFileWithpath.cpp
+++ ioFileWithpath/ioFileWithpath.cpp
@@ -31,6 +31,26 @@ int main() {
 	// selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+
 	//membuka file dalam mode membaca
 	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "unable to open file";
+	return 0;
 }
